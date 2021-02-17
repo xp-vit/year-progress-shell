5bb224e5adc76db4fa9d934d07a1e8a7aabddd79
Viktar Patotski <xp.vit.blr@gmail.com>
2021-02-17 10:26:36 +0100

    Add "the" article.

--- year-progress.sh
+++ year-progress.sh
@@ -7,4 +7,4 @@ StartYear=$(date -d "$currentYear/1/1 00:00:00" +"%s")
 Current=$(date +"%s")
 Current=$Current+".1"
 
-printf '%.2f percent of year is gone. Hurry up with your plans!\n' "$(( (Current - StartYear)*100/(EndYear - StartYear) ))"
+printf '%.2f percent of the year is gone. Hurry up with your plans!\n' "$(( (Current - StartYear)*100/(EndYear - StartYear) ))"
